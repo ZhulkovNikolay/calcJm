9ddc0ceb18dab8e91318c6a97f1025d3c214db51
Nikolay <wed10@yandex.ru>
2020-11-16 18:27:31 +0300

    First commit

---
+++ src/com/calcJm/Main.java
@@ -0,0 +1,11 @@
+package com.calcJm;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hello World");
+    }
+}
